aa5a02abaacfa510abb56d920da84c0feda46f96
YenHsiangHuang <yh@yhhuang.me>
2018-06-15 10:46:46 -0400

    Add 5.5

---
+++ chap5_BitManipulation/5.5_Debugger.cc
@@ -0,0 +1,11 @@
+// What it does?  (n & (n-1)) == 0
+#include <stdio.h>
+
+bool isPowerOf2(int n) {
+    return (n & (n-1)) == 0;
+}
+
+int main()
+{
+    printf("It checks whether n is the power of 2.\n");
+}
